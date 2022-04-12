93edfbf756b172149b2fe0c996759098af6d9983
kubilay <kubidik27@gmail.com>
2022-04-12 13:47:31 +0300

    eslint installed and integrated

--- register/src/index.js
+++ register/src/index.js
@@ -3,7 +3,6 @@ import ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 root.render(<App name="kubilay"/>);
