f0b6ba013fed4b85062302a1f4d218a04b48b45f
kubilay <kubidik27@gmail.com>
2022-04-12 17:31:20 +0300

    Left overlay responsive

--- register/src/components/OverlayLeft.js
+++ register/src/components/OverlayLeft.js
@@ -1,15 +1,63 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../constants/logos/PatikaDev";
 import style from "../styles/overlayleft.module.scss";
 import TopVector from "../constants/vectors/TopVector";
 import BottomVector from "../constants/vectors/BottomVector";
+import useWindowSize from "../hooks/useWindowSize";
 
 function OverlayLeft() {
+  const [,height ] = useWindowSize();
+  const [logoSize,setLogoSize] = useState({
+    width: 341,
+    height: 144
+  });
+  const [topVectorSize,setTopVectorSize] = useState({
+    width: 361,
+    height: 136
+  });
+  const [bottomVectorSize,setBottomVectorSize] = useState({
+    width: 521,
+    height: 136
+  });
+
+  useEffect(() => {
+    if (height < 1080) {
+      console.log(height);
+      setLogoSize({
+        width: 250,
+        height: 100
+      });
+      setTopVectorSize({
+        width: 270,
+        height: 110
+      });
+      setBottomVectorSize({
+        width:450,
+        height: 110
+      });
+    }else {
+      console.log("height", height);
+
+      setLogoSize({
+        width: 341,
+        height: 144
+      });
+      setTopVectorSize({
+        width: 361,
+        height: 136
+      });
+      setBottomVectorSize({
+        width: 521,
+        height: 136
+      });
+    }
+  }, [height]);
+
   return (
     <div className={`${style.overlayLeft} ${style.dark}`}>
       <div className={style.logo}>
-        <Logo width={341} height={144} textColor= "#444AFF" iconColor="#FFBF5E"/>
+        <Logo width={logoSize.width} height={logoSize.height} textColor= "#444AFF" iconColor="#FFBF5E"/>
       </div>
       <div className={style.title}>YAZILIM PATİKALARI </div>
       <p>
@@ -17,10 +65,10 @@ function OverlayLeft() {
       </p>
       <div className={style.vectorContainer}>
         <span>
-          <TopVector color="blue"/>
+          <TopVector width={topVectorSize.width} height={topVectorSize.height} color="blue"/>
         </span>
         <span>
-          <BottomVector color="blue"/>
+          <BottomVector width={bottomVectorSize.width} height={bottomVectorSize.height} color="blue"/>
         </span>
       </div>
     </div>
